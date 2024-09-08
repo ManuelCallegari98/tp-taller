f9e939a65f22a516b3d87a216514099abb52365a
Manuel Callegari <manuelcallegari98@gmail.com>
2024-09-08 16:17:32 -0300

    1

--- Cliente/src/app/layout.tsx
+++ Cliente/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-          <Login/>
+
       <ThemeProvider
             attribute="class"
             defaultTheme="system"
